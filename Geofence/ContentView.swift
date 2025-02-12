87e489981d2c2640dade29940d0c07781a248236
Shekhar9398 <Shekhar9398@gmail.com>
2025-02-12 15:16:21 +0530

    Initial Commit

---
+++ Geofence/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Geofence
+//
+//  Created by Omkar Zanjale on 12/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Geofence/GeofenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeofenceApp.swift
+//  Geofence
+//
+//  Created by Omkar Zanjale on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct GeofenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
